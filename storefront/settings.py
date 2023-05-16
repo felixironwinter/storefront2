fe5315c92b8f3873d8145750e8029c1b16db2128
felixironwinter <tjwinterflood@gmail.com>
2023-05-16 22:25:05 +0100

    staticfilesstorage

--- storefront/settings.py
+++ storefront/settings.py
@@ -173,4 +173,6 @@ REST_FRAMEWORK ={
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 django_heroku.settings(locals())
