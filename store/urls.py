949d4d4a3b2ca73ceff3ad93008c4c693317ef14
felixironwinter <tjwinterflood@gmail.com>
2023-09-01 23:58:33 +0100

    Adding filtering and Nested URL Routers

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path
+from django.urls import path,include
+# from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
+
+
+
+router = routers.DefaultRouter()
+
+router.register('products',views.ProductViewSet,basename='products') # basename is prefix as we will have both product-reviews-list and product-reviews-detail
+router.register('collections',views.CollectionViewSet)
+
+products_router = routers.NestedDefaultRouter(router, 'products',lookup='product')
+products_router.register('reviews',views.ReviewViewSet,basename='product-reviews')   # basename is prefix as we will have both product-reviews-list and product-reviews-detail
 
 # URLConf
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
-    path('collections/<int:pk>/', views.collection_detail,name='collection-detail'),
-]
+    path('',include(router.urls)),
+    path('',include(products_router.urls)),
+               ]
+
